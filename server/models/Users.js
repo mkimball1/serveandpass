22a1b4b74b7e158ea7b1df4330b8dba1008a395d
mkimball1 <lemortyjordan@gmail.com>
2023-11-15 01:08:55 -0800

    Connected to MongoDB Atlas cloud server instead of hosting DB locally

--- server/models/Users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const UserSchema = new mongoose.Schema({
-    username: String,
-    sessions: Array
-})
-
-const UserModel = mongoose.model("Users", UserSchema)
-module.exports = UserModel
---
+++ src/App.js
@@ -0,0 +1,142 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Session from './Session';
+import User from './User';
+import Legend from './Legend';
+import Graph from "./Graph";
+import axios from 'axios'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  const [show_legend, updateLegend] = useState(false);
+  const [username, setUsername] = useState("");
+  
+  const [currUser, setCurrUser] = useState();
+  const [currSession, setCurrSession] = useState();
+  const [listUsers, setlistUsers] = useState([])
+
+  // const notify = () => toast("Wow so easy!");
+
+  function createUser(){
+    const data = {
+      "username": username,
+      "sessions": []
+    }
+    axios.post('https://us-east-1.aws.data.mongodb-api.com/app/serveandpass-wkoqd/endpoint/createUser', data)
+      .then(response => {
+          console.log('Response:', response.data);
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      });
+  }
+
+  const addSessionToUser = (newSessionList) => {
+    const updatedUser = new User(currUser.username);
+    updatedUser.sessions = [...currUser.sessions, newSessionList];
+
+    console.log("updatedUser:", updatedUser)
+    axios.put("https://us-east-1.aws.data.mongodb-api.com/app/serveandpass-wkoqd/endpoint/editUser?username="+updatedUser.username, updatedUser)
+        .then(response => {
+          console.log('Response:', response.data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    setCurrUser(updatedUser);
+  };
+
+  useEffect(() => {
+    axios.get('https://us-east-1.aws.data.mongodb-api.com/app/serveandpass-wkoqd/endpoint/getUsers')
+        .then((response) => {
+            setlistUsers(response.data)
+            console.log('Data retrieved:', response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+  }, [])
+
+
+  
+  function updateInputUsername(event) {
+    setUsername(event.target.value);
+  }
+
+  const incrementPass = (passKey) => {
+    const updatedSession = { ...currSession };
+    updatedSession.passes = {
+        ...currSession.passes,
+        [passKey]: currSession.passes[passKey] + 1,
+    };
+    updatedSession.total += passKey;
+    updatedSession.count += 1;
+    updatedSession.average = (updatedSession.total / updatedSession.count);
+    setCurrSession(updatedSession)
+  }
+
+  function initAll(event) {
+    event.preventDefault();
+    setCurrUser(null);
+    const foundUser = listUsers.find(user => user['username'] === username);
+    if (foundUser) {
+      toast("Loading User: " + username)
+      setCurrUser(foundUser);
+    } else {
+      toast("Creating New User: " + username)
+      setCurrUser(new User(username));
+      createUser()
+    }
+    setCurrSession(new Session(new Date().toLocaleDateString()));
+  }
+
+  return(
+    <div>
+      <ToastContainer />
+      <form onSubmit={initAll}>
+        <input type="text" value={username} onChange={updateInputUsername} placeholder="User Id"/>
+        <button type="submit">Submit</button>
+      </form>
+
+      {currUser? 
+        <div>
+          <p> Current User: {currUser.username}</p> 
+          <div className="buttonContainer">
+            <button onClick={() => incrementPass(3)}> 3 </button>
+            <button onClick={() => incrementPass(2)}> 2 </button>
+            <button onClick={() => incrementPass(1)}> 1 </button>
+            <button onClick={() => incrementPass(0)}> 0 </button>
+            <button onClick={() => setCurrSession(new Session(new Date().toLocaleDateString()))}> Clear </button>
+          </div>
+          <div className="currSessionStats">
+              <p> 3: {currSession.passes["3"]}</p>
+              <p> 2: {currSession.passes["2"]}</p>
+              <p> 1: {currSession.passes["1"]}</p>
+              <p> 0: {currSession.passes["0"]}</p>
+              <p> Current Session Average: {currSession.average.toFixed(2)}</p>
+              <p> Current Session Total Passes: {currSession.count} </p>
+          </div>
+          {/* TODO */}
+          <button onClick={() => {
+            //update DB
+            addSessionToUser(currSession)
+            //RESET Current Session
+            setCurrSession(new Session(new Date().toLocaleDateString()))
+            }}> Submit Session </button>
+
+
+          <div className="legend">
+            <button onClick={() => updateLegend(!show_legend)}> Show Legend </button>
+            {show_legend ? <Legend/> : null}
+          </div>
+          <Graph my_data={currUser.sessions}/>
+        </div>
+      :null}
+    </div>
+    
+  );
+}
+
+export default App;
